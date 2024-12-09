e607b1b4e0571eef5a5e9f76db68188c6d83cfe8
Salman-Nazeer <nazeersalman716@gmil.com>
2024-12-09 18:41:02 -0500

    All product uploded

--- Front-End/src/components/Adminpanel/adminProductCard.jsx
+++ Front-End/src/components/Adminpanel/adminProductCard.jsx
@@ -7,25 +7,26 @@ const adminProductCard = ({ data, fetchData }) => {
   const [editProduct, setEditProduct] = useState(false);
   return (
     <div className="bg-white p-4 rounded ">
-      <div className="w-40 h-[100%] flex flex-col justify-between">
-        <div className="h-34 flex justify-center items-center">
+      <div className="w-32 flex flex-col h-full">
+        <div className="w-32 h-36 flex justify-center items-center">
           <img
             src={data?.productImage[0]}
-            alt=""
-            className="mx-auto object-fill h-full"
+            className="mx-auto w-full object-contain h-full"
           />
         </div>
+
         <h1 className="text-ellipsis line-clamp-2">{data?.productName}</h1>
 
         <p className="font-semibold">{displayPKRCurrency(data?.price)}</p>
 
         <div
-          className="w-fit ml-auto p-2 bg-green-100 hover:bg-green-600 rounded-full cursor-pointer hover:text-white"
+          className="w-fit ml-auto mt-auto p-2 bg-green-100 hover:bg-green-600 mb-0 rounded-full cursor-pointer hover:text-white"
           onClick={() => setEditProduct(true)}
         >
           <MdModeEditOutline />
         </div>
       </div>
+
       {editProduct && (
         <AdminEditProduct
           productData={data}
